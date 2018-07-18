6f49e385e90062d5665d9e2e01486435332293a1
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2018-07-18 09:16:41 +0530

    added changes

---
+++ app/src/main/java/com/prudhvireddy/bakingapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.prudhvireddy.bakingapp.utils;
+
+public class Constants {
+
+    public static final String MyPREFERENCES = "myprefs";
+    public static final String RECEIPE_NAME = "receipe_name";
+    public static final String RECEIPE_POSITION = "receipe_position";
+}
